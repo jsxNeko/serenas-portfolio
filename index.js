7cee4186d53557d7a27f01d5c7e302ecbf050fff
Serenuy <dserena01@yahoo.com>
2022-07-25 16:15:34 -0500

    Major changes to css/html

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let slideIndex = 1;
 const projImg = document.querySelector('.image-holder');
 const projshortDes = document.querySelector('.proj-short-des');
-projshortDes.innerHTML = projDesc[0];
 const projImages = ['https://image.thum.io/get/maxAge/12/width/700/https://serenuy.github.io/Travel-Dreams/index.html', 'https://i.imgur.com/lU8RFd8.png', 'https://i.imgur.com/nI7aCmS.png'];
 const projLinks = ['https://serenuy.github.io/Travel-Dreams/','https://serenuy.github.io/Revanents','https://expertece-draft1234.herokuapp.com/'];
 const projDesc = ['Travel Landing Page<br> <em>Node.<span style="color:green;">js</span>, Vanilla <span style="color:yellow;">JS</span> + <span style="color: orange;">HTML</span> + <span style="color:blue;">CSS</span>, <span style="color:purple;">Bootstrap 5<span></em>', 'Revanents Directory<br> <em>Node.<span style="color:green;">js</span>, <span style="color:lightblue;">ReactJS</span>, Vanilla <span style="color:blue;">CSS</span></em>', 'E-commerce for ExperTece, a small business <br> <em>Node.<span style="color:green;">js</span>, <span style="color:orange;">Firebase</span>, <span style="color:plum">PHP</span>, Vanilla <span style="color:yellow;">JS</span> + <span style="color: orange;">HTML</span> + <span style="color:blue;">CSS</span>, <span style="color:purple;">Bootstrap 5</span></em>'];
-
+projshortDes.innerHTML = projDesc[0];
+projImg.style.backgroundImage = `url(${projImages[0]})`;
 
 const plusSlide = (n) => {
 	if(slideIndex <= 3) slideIndex += n;
@@ -22,7 +22,7 @@ projImg.onclick = () => {
 	window.open(projLinks[slideIndex-1], '_blank')
 };
 
-const scrollDown = (element) => {
+const scrolltoView = (element) => {
 	let scrollto = document.querySelector(element);
 	scrollto.scrollIntoView({
 		behavior:'smooth',
