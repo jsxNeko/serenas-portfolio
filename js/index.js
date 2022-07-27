cebc23296e78418603bbe6eb75058803a7038791
Serenuy <dserena01@yahoo.com>
2022-07-27 12:17:49 -0500

    Fixed slideshow bug

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const projshortDes = document.querySelector('.proj-short-des');
 const projImages = ['https://image.thum.io/get/maxAge/12/width/700/https://serenuy.github.io/Travel-Dreams/index.html', 'https://i.imgur.com/lU8RFd8.png', 'https://i.imgur.com/nI7aCmS.png'];
 const projLinks = ['https://serenuy.github.io/Travel-Dreams/','https://serenuy.github.io/Revanents','https://expertece-draft1234.herokuapp.com/'];
 const projDesc = ['Travel Landing Page<br> <em>Node.<span style="color:green;">js</span>, Vanilla <span style="color:yellow;">JS</span> + <span style="color: orange;">HTML</span> + <span style="color:blue;">CSS</span>, <span style="color:purple;">Bootstrap 5<span></em>', 'Revanents Directory<br> <em>Node.<span style="color:green;">js</span>, <span style="color:lightblue;">ReactJS</span>, Vanilla <span style="color:blue;">CSS</span></em>', 'E-commerce for ExperTece, a small business <br> <em>Node.<span style="color:green;">js</span>, <span style="color:orange;">Firebase</span>, <span style="color:plum">PHP</span>, Vanilla <span style="color:yellow;">JS</span> + <span style="color: orange;">HTML</span> + <span style="color:blue;">CSS</span>, <span style="color:purple;">Bootstrap 5</span></em>'];
+
 projshortDes.innerHTML = projDesc[0];
 projImg.style.backgroundImage = `url(${projImages[0]})`;
 
@@ -14,8 +15,15 @@ const plusSlide = (n) => {
 };
 
 const showSlide = (n) => {
-	projshortDes.innerHTML = projDesc[n-1];
-	projImg.style.backgroundImage = `url(${projImages[n-1]})`;	
+	if(n < 1) {
+		projshortDes.innerHTML = projDesc[2];
+		projImg.style.backgroundImage = `url(${projImages[2]})`;
+		slideIndex = 3;
+		return;
+	} else {	
+		projshortDes.innerHTML = projDesc[n-1];
+		projImg.style.backgroundImage = `url(${projImages[n-1]})`;
+	}
 };
 
 projImg.onclick = () => {
